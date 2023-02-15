d16d882f7ded57702fc4c538992e5bf2a22565c1
Lukáš Janáček <ljanacek@netvoice.cz>
2023-02-15 15:11:10 +0100

    Initial commit.

---
+++ src/main/kotlin/cz/upce/fei/janacek/main/Nnpia01Application.kt
@@ -0,0 +1,11 @@
+package cz.upce.fei.janacek.main
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Nnpia01Application
+
+fun main(args: Array<String>) {
+    runApplication<Nnpia01Application>(*args)
+}
---
+++ src/test/kotlin/cz/upce/fei/janacek/main/Nnpia01ApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.upce.fei.janacek.main
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Nnpia01ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
