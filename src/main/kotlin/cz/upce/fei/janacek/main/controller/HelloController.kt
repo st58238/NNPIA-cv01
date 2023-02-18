29a9701c45dbde99f23b440c1c26a95e81933180
Lukáš Janáček <ljanacek@netvoice.cz>
2023-02-18 19:22:10 +0100

    Controller addition.

---
+++ src/main/kotlin/cz/upce/fei/janacek/main/controller/HelloController.kt
@@ -0,0 +1,37 @@
+package cz.upce.fei.janacek.main.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Controller slouží jako komponenta MVC modelu, stejně jako RestController.
+ * Rozdíl je v tom, že RestController automaticky, na pozadí, přidává dodatečnou anotaci ResponseBody
+ *  a zároveň automaticky převádí response na JSON/XML.
+ */
+@RestController
+@RequestMapping("/api/v1")
+class HelloController {
+
+    @GetMapping("")
+    fun helloWorld(): String {
+        return "Hello world from Spring Boot application."
+    }
+
+    @GetMapping("path")
+    fun returnString(@RequestParam path: String): String {
+        return "Got: $path"
+    }
+
+    @GetMapping("query")
+    fun returnQuery(@RequestParam query: String): String {
+        return "Query: $query"
+    }
+
+    /**
+     * V Javě bych využil buďto třídní anotaci @Data, nebo bych k atributům přidal anotace @Getter a @Setter,
+     *  případně ještě @ToString a @EqualsAndHashCode.
+     */
+
+}
